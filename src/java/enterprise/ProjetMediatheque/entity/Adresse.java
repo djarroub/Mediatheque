dfe1bc4b22da66149035109a38a871170a55bafb
Gilles <Gilles@Tang>
2012-10-31 11:58:18 +0100

    + Création classe Adresse

---
+++ src/java/enterprise/ProjetMediatheque/entity/Adresse.java
@@ -0,0 +1,14 @@
+package enterprise.ProjetMediatheque.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Gilles
+ */
+@Entity
+@Table (name="ADRESS")
+public class Adresse {
+    
+}
