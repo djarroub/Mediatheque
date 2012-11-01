1e983e41837b8745d4bc426e31b37ff17c114eac
Guyader Erwan <erwanguyader@gmail.com>
2012-11-02 00:02:40 +0100

    Modification des Entités Adherent, Emprunt et Reservation

--- src/java/enterprise/ProjetMediatheque/entity/Emprunt.java
+++ src/java/enterprise/ProjetMediatheque/entity/Emprunt.java
@@ -4,10 +4,37 @@
  */
 package enterprise.ProjetMediatheque.entity;
 
+import javax.persistence.OneToOne;
+import java.util.Date;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import static javax.persistence.TemporalType.DATE;
+
 /**
  *
  * @author guyader
  */
+@Entity
 public class Emprunt {
+    @Id
+    @Column(name = "ID")
+    private int id;
     
+    @Column(name = "BEGINNING_DATE")
+    @Temporal(DATE)
+    private Date dateDebut;
+    
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Adherent adherent;
+    
+    @OneToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
 }
--- src/java/enterprise/ProjetMediatheque/entity/Reservation.java
+++ src/java/enterprise/ProjetMediatheque/entity/Reservation.java
@@ -4,10 +4,32 @@
  */
 package enterprise.ProjetMediatheque.entity;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 /**
  *
  * @author guyader
  */
 public class Reservation {
+    @Id
+    @Column(name = "ID")
+    private int id;
     
+    @Column(name = "RESERVATION_DATE")
+    private Date dateDeReservation;
+    
+    @Column(name = "EXPIRY_DATE")
+    private Date dateDExpiration; // renommee depuis DateFinDeValidite
+    
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Adherent adherent;
+    
+    @ManyToOne
+    @JoinColumn(name = "WORK_ID")
+    private Ouvrage ouvrage;
 }
