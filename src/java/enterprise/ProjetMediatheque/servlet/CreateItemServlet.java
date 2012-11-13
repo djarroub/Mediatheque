ec7c90fa627360c3831c01de7e375d56689b908b
Gilles <Gilles@Tang>
2012-11-14 00:27:00 +0100

    Aaaahahahahaahahh !!! Item implementeed!

--- src/java/enterprise/ProjetMediatheque/servlet/CreateItemServlet.java
+++ src/java/enterprise/ProjetMediatheque/servlet/CreateItemServlet.java
@@ -9,6 +9,7 @@ import enterprise.ProjetMediatheque.entity.Item;
 import enterprise.ProjetMediatheque.entity.Ouvrage;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -24,58 +25,94 @@ import javax.transaction.UserTransaction;
  *
  * @author Gilles
  */
-@WebServlet(name = "CreateItemServlet", urlPatterns = {"/CreateItemServlet"})
+@WebServlet(name = "CreateItemServlet", urlPatterns = {"/CreateItem"})
 public class CreateItemServlet extends HttpServlet {
 
     @PersistenceUnit
     private EntityManagerFactory emf;
     @Resource
     private UserTransaction utx;
-    
     private String req_ouvrage = "";
     private String req_nbExemplaire = "";
-    
-    
+
     /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
+     * Handles the HTTP
+     * <code>GET</code> method.
      *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         assert emf != null;  //Make sure injection went through correctly.
         EntityManager em = null;
         try {
+            em = emf.createEntityManager();
+            
+            List ouvrages = em.createQuery("select a from Ouvrage a").getResultList();
+            request.setAttribute("ouvrages", ouvrages);
+            
+            request.getRequestDispatcher("createItem.jsp").forward(request, response);
+        } catch (Exception ex) {
+            throw new ServletException(ex);
+        } finally {
+            //close the em to release any resources held up by the persistebce provider
+            if(em != null) {
+                em.close();
+            }
+        }
 
-            //Get the data from user's form
-            req_ouvrage = (String) request.getParameter("ouvrage");
-            req_nbExemplaire = (String) request.getParameter("nbExemplaire");
-            
-            if (req_ouvrage.isEmpty()) {
-                returnMessageError(request, response, "Veuillez s&eacute;lectionner un ouvrage!");
-            } else if (req_nbExemplaire.isEmpty()) {
-                returnMessageError(request, response, "Veuillez entrer un nombre d'exemplaire !");
-            }
-            
-            
-            Ouvrage ouvrage = em.find(Ouvrage.class, Long.parseLong(req_ouvrage));
-            
-            if(ouvrage == null){
-                returnMessageError(request, response, "Un probl&egrave;me est survenu avec l'importation de l'ouvrage !");
-            }
-            
-            Item item = new Item(ouvrage);
-            
+
+
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        assert emf != null;  //Make sure injection went through correctly.
+        EntityManager em = null;
+        try {
             //begin a transaction
             utx.begin();
             //create an em. 
             //Since the em is created inside a transaction, it is associsated with 
             //the transaction
             em = emf.createEntityManager();
+            
+            //Get the data from user's form
+            req_ouvrage = (String) request.getParameter("ouvrage");
+            req_nbExemplaire = (String) request.getParameter("nbExemplaire");
+
+            if (req_ouvrage.isEmpty()) {
+                returnMessageError(request, response, "Veuillez s&eacute;lectionner un ouvrage!");
+            } else if (req_nbExemplaire.isEmpty()) {
+                returnMessageError(request, response, "Veuillez entrer un nombre d'exemplaire !");
+            }
+
+
+            Ouvrage ouvrage = null;
+            ouvrage = em.find(Ouvrage.class, Long.parseLong(req_ouvrage));
+
+            if (ouvrage == null) {
+                returnMessageError(request, response, "Un probl&egrave;me est survenu avec l'importation de l'ouvrage !");
+            }
+
+            Item item = new Item(ouvrage);
+            if (item == null) {
+                returnMessageError(request, response, "Un probl&egrave;me est survenu avec la creation de l'item !");
+            }
+            
+            
             //persist the person entity
             em.persist(item);
             //commit transaction which will trigger the em to 
@@ -95,37 +132,6 @@ public class CreateItemServlet extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP
-     * <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
     /**
      * Returns a short description of the servlet.
      *
@@ -134,17 +140,17 @@ public class CreateItemServlet extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
-    
-    
+    }
+
     /**
      * Retourne une message d'erreur a la page createItem.jsp
+     *
      * @param request
      * @param response
      * @param msg
      */
     private void returnMessageError(HttpServletRequest request, HttpServletResponse response, String msg) throws ServletException, IOException {
-        request.setAttribute("alert",           "<span class=\"alert\">" + msg + "</span>");
+        request.setAttribute("alert", "<span class=\"alert\">" + msg + "</span>");
         request.getRequestDispatcher("createItem.jsp").forward(request, response);
     }
 }
---
+++ src/java/enterprise/ProjetMediatheque/servlet/InitWorkServlet.java
@@ -0,0 +1,110 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enterprise.ProjetMediatheque.servlet;
+
+import enterprise.ProjetMediatheque.entity.Auteur;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author Gilles
+ */
+@WebServlet(name = "InitWorkServlet", urlPatterns = {"/InitWorkServlet"})
+public class InitWorkServlet extends HttpServlet {
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+    
+    @Resource
+    private UserTransaction utx;
+    int nbOuvrage = 50;
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       assert emf != null;  //Make sure injection went through correctly.
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = emf.createEntityManager();
+            
+            Query query = em.createQuery("delete from Ouvrage o");
+            query.executeUpdate();
+            
+            /*for (int i = 0; i < nbAuthor; i++) {
+                em.persist(new Auteur(getRandomNom(), getRandomNom()));
+            }*/
+            utx.commit();
+        } catch (Exception ex) {
+            throw new ServletException(ex);
+        } finally {
+            //close the em to release any resources held up by the persistebce provider
+            if (em != null) {
+                em.close();
+                response.sendRedirect("ListAuthors");
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
